466b35331c24e1bb2eed1c3abd78c7e59009a67c
Elaine Young <elaineyoung702@gmail.com>
2019-10-01 15:26:07 -0700

    completed lemmings challenge. will circle back for the advanced solution

--- lemmings.py
+++ lemmings.py
@@ -17,25 +17,33 @@ Find the farthest any single lemming needs to travel for food.
     >>> furthest(7, [0, 6])
     3
 
-    >>> furthest_optimized(7, [0, 6])
-    3
-
-    >>> furthest_optimized(3, [0, 1, 2])
-    0
-
-    >>> furthest_optimized(3, [2])
-    2
-
-    >>> furthest_optimized(3, [0])
-    2
-
-    >>> furthest_optimized(6, [2, 4])
-    2
 """
 
 
 def furthest(num_holes, cafes):
     """Find longest distance between a hole and a cafe."""
+    final_list = []
+
+    for i in range(num_holes):
+        worst = None
+        travel = []
+
+        for cafe in cafes:
+            if worst is None:
+                worst = abs(cafe - i)
+
+            elif abs(cafe - i) < worst:
+                worst = abs(cafe - i)
+
+            travel.append(worst)
+            
+        final_list.append(min(travel))
+
+    final = max(final_list)
+
+    return final
+
+
 
 if __name__ == '__main__':
     import doctest
