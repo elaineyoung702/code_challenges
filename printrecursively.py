de7b2d93d28b619a26d6f5c66c3e116ea4f28cd1
Elaine Young <elaineyoung702@gmail.com>
2019-10-10 14:22:38 -0700

    completed printrecursively challenge

---
+++ printrecursively.py
@@ -0,0 +1,29 @@
+"""Print items in the list, using recursion.
+
+For example::
+
+   >>> print_recursively([1, 2, 3])
+   1
+   2
+   3
+
+"""
+
+
+def print_recursively(lst):
+    """Print items in the list, using recursion."""
+
+    if not lst:
+        return
+
+
+    print(lst[0])
+    lst.pop(0)
+
+    print_recursively(lst)
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TEST PASSED. GREAT JOB!\n")
