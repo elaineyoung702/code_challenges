d16c0be87b29e0d01af04606041bb494a874171b
Elaine Young <elaineyoung702@gmail.com>
2019-11-18 13:18:32 -0800

    removed variable names and changed operator for division

--- calculator.py
+++ calculator.py
@@ -26,32 +26,29 @@ def calc(s):
     """Evaluate expression."""
 
     s = s.split(" ")
-    num1 = s[-1]
-    num2 = s[-2]
+
     if s[-3] == "+":
-        final_val = int(num1) + int(num2)
+        final_val = int(s[-1]) + int(s[-2])
     elif s[-3] == "-":
-        final_val = int(num2) - int(num1)
+        final_val = int(s[-2]) - int(s[-1])
     elif s[-3] == "*":
-        final_val = int(num1) * int(num2)
+        final_val = int(s[-1]) * int(s[-2])
     elif s[-3] == "/":
-        final_val = int(num2) / int(num1)
+        final_val = int(s[-2]) // int(s[-1])
 
     if len(s) <= 3:
         return final_val
 
-    num3 = s[1]
-
     if s[0] == "+":
-        final_val += int(num3)
+        final_val += int(s[1])
     elif s[0] == "-":
-        final_val = int(num3) - final_val
+        final_val = int(s[1]) - final_val
     elif s[0] == "*":
-        final_val = final_val * int(num3)
+        final_val = final_val * int(s[1])
     elif s[0] == "/":
-        final_val = int(num3) / final_val
+        final_val = int(s[1]) // final_val
 
-    return int(final_val)
+    return final_val
 
 if __name__ == '__main__':
     import doctest
